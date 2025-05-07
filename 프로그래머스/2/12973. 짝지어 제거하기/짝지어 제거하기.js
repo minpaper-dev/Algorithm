ae6c6e8f247a05ba9c88552e8cb6bc19b2c15aaa
MINJI YUN <137516352+minpaper-dev@users.noreply.github.com>
2025-05-07 11:55:45 +0900

    [level 2] Title: 짝지어 제거하기, Time: 69.12 ms, Memory: 57.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,12 @@
+function solution(s) {
+    
+    
+    const stack = []
+    
+    for(let i of s){
+        if(stack.length && stack[stack.length - 1] === i) stack.pop()
+        else stack.push(i)
+    }
+    
+    return !stack.length ? 1 : 0
+}
